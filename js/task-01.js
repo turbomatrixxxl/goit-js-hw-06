d56c5a3a3f71c5ca1a3d22ed663ab65620bb0371
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-04-09 00:45:06 +0300

    Finished task 10 (optional)

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,14 @@
-const categories = document.querySelector('#categories');
+const categories = document.querySelector("#categories");
 // console.log(categories);
 
-const categorie = document.querySelectorAll('.item');
+const categorie = document.querySelectorAll(".item");
 // console.log(categorie);
 
-categorie.forEach(element => {
-  const category = element.querySelector('h2').textContent;
+categorie.forEach((element) => {
+  const category = element.querySelector("h2").textContent;
   //   console.log(category);
-  const subCategory = element.querySelector('ul').querySelectorAll('li').length;
+
+  const subCategory = element.querySelector("ul").querySelectorAll("li").length;
   //   console.log(subCategory);
 
   //   const obj = {
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,59 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("#controls");
+
+const input = controls.querySelector(`input[type="number"]`);
+
+const buttons = controls.querySelectorAll(`button`);
+// console.log(buttons);
+
+const createBtn = buttons[0];
+// console.log(createBtn);
+
+const destroyBtn = buttons[1];
+// console.log(destroyBtn);
+
+const divContainer = document.querySelector("#boxes");
+
+const color = getRandomHexColor();
+const colors = [];
+
+colors.push(color);
+// console.log(color);
+
+const addDiv = [];
+let size = [];
+let dim = 30;
+
+size.push(dim);
+// console.log(size);
+
+createBtn.addEventListener("click", () => {
+  for (let index = 0; index <= input.value - 2; index++) {
+    dim += 10;
+    size.push(dim);
+    // console.log(size);
+  }
+
+  for (let index = 0; index <= input.value - 2; index++) {
+    colors.push(getRandomHexColor(index));
+    // console.log(colors);
+  }
+
+  for (let index = 0; index <= input.value - 1; index++) {
+    addDiv.push(`<div style="background-color:${colors[index]}; width:${size[index]}px; height:${size[index]}px" class="div${index}">b${index}</div>
+`);
+    divContainer.insertAdjacentHTML("beforeend", addDiv[index]);
+  }
+
+  // console.log(addDiv.length);
+  // console.log(colors.length);
+  // console.log(divContainer);
+});
+
+destroyBtn.addEventListener("click", (event) => {
+  divContainer.innerHTML = "";
+  input.value = "";
+});
