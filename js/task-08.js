073d68b7abe42405299b7f343fe56a7eea80cd84
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-04-08 21:36:09 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+const emailInput = form.querySelector(`input[name="email"]`);
+// console.log(emailInput);
+
+const passwordInput = form.querySelector(`input[type="password"]`);
+// console.log(passwordInput);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("All fields must be completed");
+  }
+  const formElements = event.target.elements;
+  const email = formElements.email.value;
+  //   console.log(email);
+
+  const password = formElements.password.value;
+  //   console.log(password);
+
+  const formValues = {
+    email: `${email}`,
+    password: `${password}`,
+  };
+  console.log(formValues);
+  event.target.reset();
+});
