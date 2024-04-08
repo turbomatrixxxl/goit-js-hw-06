094aa53cc1c7f53ef3e0ec8b3abb2920b74b8baa
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-04-08 21:54:22 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// console.log(getRandomHexColor());
+
+const body = document.querySelector("body");
+
+const changeColorBtn = document.querySelector(".change-color");
+
+const span = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+});
