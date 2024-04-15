cf00d6fbc091c2d220efa0739110a6eb00359df6
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-04-15 23:39:03 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -22,40 +22,61 @@ const divContainer = document.querySelector("#boxes");
 const color = getRandomHexColor();
 const colors = [];
 
-colors.push(color);
-// console.log(color);
-
 const addDiv = [];
 let size = [];
 let dim = 30;
 
-size.push(dim);
-// console.log(size);
+const alertContainer = `<p class="alert"></p>
+`;
+controls.insertAdjacentHTML("beforeend", alertContainer);
+const alertMessage = controls.querySelector(".alert");
+alertMessage.style.color = "red";
 
 createBtn.addEventListener("click", () => {
-  for (let index = 0; index <= input.value - 2; index++) {
+  if (!input.value) {
+    alertMessage.textContent = "";
+    return;
+  }
+  if (input.value < 1 || input.value > 100) {
+    alertMessage.textContent = `Amount must be between 1 and 100...!`;
+    input.value = "";
+    divContainer.innerHTML = "";
+    return;
+  }
+
+  if (input.value >= 1 || input.value <= 100) {
+    alertMessage.textContent = "";
+    create(input.value);
+    input.value = "";
+  }
+});
+
+destroyBtn.addEventListener("click", destroy);
+
+function create(value) {
+  for (let index = 0; index < value; index++) {
     dim += 10;
     size.push(dim);
-    // console.log(size);
-  }
+    console.log(size);
 
-  for (let index = 0; index <= input.value - 2; index++) {
     colors.push(getRandomHexColor(index));
-    // console.log(colors);
-  }
+    console.log(colors);
 
-  for (let index = 0; index <= input.value - 1; index++) {
-    addDiv.push(`<div style="background-color:${colors[index]}; width:${size[index]}px; height:${size[index]}px"></div>
-`);
+    addDiv.push(
+      `<div style="background-color:${colors[index]}; width:${size[index]}px; height:${size[index]}px"></div>`
+    );
+    console.log(addDiv);
+
     divContainer.insertAdjacentHTML("beforeend", addDiv[index]);
   }
 
   // console.log(addDiv.length);
   // console.log(colors.length);
   // console.log(divContainer);
-});
+}
 
-destroyBtn.addEventListener("click", (event) => {
+function destroy() {
+  alertMessage.textContent = "";
   divContainer.innerHTML = "";
   input.value = "";
-});
+}
